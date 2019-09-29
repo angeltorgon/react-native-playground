068ebd9f84962186a31a0e30c5cdcaad53358fb5
angel-torres <alas4224@gmail.com>
2019-09-28 21:39:35 -0500

    was able to render bottom tab navigation system

--- react-native-playground/screens/NewNavigator.js
+++ react-native-playground/screens/NewNavigator.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import Page1 from './Page1';
 import Page2 from './Page2';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation'
 
-const MainNavigator = createStackNavigator({
-    Page1: { screen: Page1 },
-    Page2: { screen: Page2 },
+const MainNavigator = createBottomTabNavigator({
+    Page1: Page1,
+    Page2: Page2,
 });
 
 const AppWithNav = createAppContainer(MainNavigator);
